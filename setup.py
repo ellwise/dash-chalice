d2c0d5565a3d0438096a300ea4d11306d3de44f5
Elliott Wise <ell.wise@gmail.com>
2021-08-22 12:19:50 +0100

    Added readme to pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(curr_dir, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="dash-chalice",
-    version="0.1",
+    version="0.1.1",
     description="Chalice as an alternative to Flask for Plotly Dash",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,5 +19,6 @@ setup(
     license="GNU GPL v3",
     packages=["dash_chalice"],
     install_requires=["dash", "chalice"],
+    include_package_data=True,
     zip_safe=False,
 )
